cf2bb5b22bbcc6ab37b4960c34d56cc480134869
Roberto Gigli <roberto.gigli98@outlook.it>
2023-03-11 17:27:04 +0100

    Novità: - Nuova Stringa - Elimina Stringa
    Fix:
    - Fix check string, list e dict empty e None invece che solo None
    - Rimozione tema scuro

--- localization_manager_qt.py
+++ localization_manager_qt.py
@@ -18,6 +18,9 @@ class App(QApplication):
         #window
         self.window: QMainWindow = uic.loadUi("localization_manager.ui")
         
+        #mnu
+        self.mnu_modifica: QMenu = self.window.findChild(QMenu, "mnu_modifica")
+        
         #inp
         self.inp_search: QLineEdit = self.window.findChild(QLineEdit, "inp_search")
         self.inp_string_content: QPlainTextEdit = self.window.findChild(QPlainTextEdit, "inp_string_content")
@@ -30,6 +33,9 @@ class App(QApplication):
         self.act_open_arb: QAction = self.window.findChild(QAction, "act_open_arb")
         self.act_save_arb: QAction = self.window.findChild(QAction, "act_save_arb")
         self.act_save_arb_as: QAction = self.window.findChild(QAction, "act_save_arb_as")
+        self.act_new_string: QAction = self.window.findChild(QAction, "act_new_string")
+        self.act_rename_string: QAction = self.window.findChild(QAction, "act_rename_string")
+        self.act_delete_string: QAction = self.window.findChild(QAction, "act_delete_string")
         
         #btn
         self.btn_translate: QPushButton = self.window.findChild(QPushButton, "btn_translate")
@@ -44,11 +50,15 @@ class App(QApplication):
         
         #lsw
         self.lsw_strings.itemClicked.connect(self.load_current_string)
+        self.lsw_strings.addActions(self.mnu_modifica.actions())
         
         #act
         self.act_open_arb.triggered.connect(self.open_arbfile)
         self.act_save_arb.triggered.connect(self.save_arbfile)
         self.act_save_arb_as.triggered.connect(self.save_arbfile_as)
+        self.act_new_string.triggered.connect(self.new_string)
+        self.act_rename_string.triggered.connect(self.rename_string)
+        self.act_delete_string.triggered.connect(self.delete_string)
         
         #btn
         self.btn_translate.clicked.connect(self.translate)
@@ -76,17 +86,18 @@ class App(QApplication):
     
     def load_arbfile(self) -> None:
         self.arb_data = self.get_arbfile()
-        if not self.arb_data:
+        if self.arb_data == None:
             print("File non valido")
             return
 
         print(self.arb_data)
 
-        self.set_list(self.arb_data.keys())
+        self.reset_list()
+        self.inp_string_content.setPlainText("")
         pass
     
     def save_arbfile(self) -> None:
-        if not self.arb_data:
+        if self.arb_data == None:
             self.arb_data = {}
 
         if not self.current_arbfile_path:
@@ -115,12 +126,12 @@ class App(QApplication):
     
     def load_current_string(self, selected_item: QListWidgetItem) ->None:
 
-        if not self.arb_data:
+        if self.arb_data == None:
             return
 
         string_data = self.arb_data[selected_item.text()]
 
-        if not string_data:
+        if string_data == None:
             return
 
         print(f"loading string {selected_item.text()}")
@@ -133,7 +144,7 @@ class App(QApplication):
         if not selected_items or len(selected_items) == 0 or not current_string:
             return
 
-        if not self.arb_data:
+        if self.arb_data == None:
             return
 
         # Prendo solo la prima stringa selezionata
@@ -157,13 +168,13 @@ class App(QApplication):
             return arbfile_content
         
     def reset_list(self) -> None:
-        if not self.arb_data:
+        if self.arb_data == None:
             return
 
         self.set_list(self.arb_data.keys())
         
     def set_list(self, strings: Union[list[str], None]) -> None:
-        if not strings:
+        if strings == None:
             return
         
         rows = self.lsw_strings.count()
@@ -186,7 +197,7 @@ class App(QApplication):
             self.reset_list()
             return
 
-        if not self.arb_data:
+        if self.arb_data == None:
             return
 
         result = [(k, v) for k, v in self.arb_data.items()
@@ -195,6 +206,77 @@ class App(QApplication):
         
         self.set_list(filtered_arb.keys())
         self.inp_string_content.setPlainText("")
+        
+    def new_string(self) -> None:
+        if self.arb_data == None:
+            self.arb_data = {}
+            
+            
+        new_string_name = self.text_from_user("Nome stringa nuova")
+        
+        #L'utente ha annullato l'operazione
+        if new_string_name == None:
+            return
+        
+        if not new_string_name:
+            self.alert_user("Nome della stringa non valido")
+            return
+        
+        if new_string_name in self.arb_data.keys():
+            self.alert_user("Questa stringa già esiste")
+            print("This string already exist")
+            return
+            
+        current_item: Union[QListWidgetItem, None] = self.lsw_strings.currentItem()
+        current_key: Union[str, None] = current_item.text() if current_item else None
+        current_pos: Union[int, None] = list(self.arb_data.keys()).index(current_key) if current_key else None
+        
+        items = list(self.arb_data.items())
+        
+        if current_pos:
+            items.insert(current_pos + 1, (new_string_name, ""))
+            self.arb_data = dict(items)
+        else:
+            self.arb_data[new_string_name] = ""
+            
+        self.reset_list()
+        self.inp_string_content.setPlainText("")
+        print("Hello new string")
+        pass
+    
+    def rename_string(self) -> None:
+        print("Hello rename string")
+        pass
+    
+    def delete_string(self) -> None:
+        if self.arb_data == None:
+            self.alert_user("Nessuna file di stringe")
+            print("No arb data")
+            return
+        
+        selected_items: Union[list[QListWidgetItem], None] = self.lsw_strings.selectedItems()
+        
+        current_item: Union[QListWidgetItem, None] = selected_items[0] if selected_items else None
+        current_key: Union[str, None] = current_item.text() if current_item else None
+        
+        if not current_key:
+            self.alert_user("Nessuna stringa selezionata da eliminare")
+            print("No key to delete")
+            return
+        
+        self.arb_data.pop(current_key)
+        self.reset_list()
+        self.inp_string_content.setPlainText("")
+        print("Hello delete string")
+        pass
+    
+    def alert_user(self, message: str) -> None:
+        message = QMessageBox.information(self.window, self.window.windowTitle(), message)
+        pass
+    
+    def text_from_user(self, message: str, text: str = "") -> Union[str, None]:
+        result, ok = QInputDialog.getText(self.window, self.window.windowTitle(), message, text=text)
+        return result.strip() if ok else None
     
     def translate(self) -> None:
         print("Hello Translate")
